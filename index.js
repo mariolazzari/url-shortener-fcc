f9a4e26a8384d0931705dff34e75f42f9589f473
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-18 20:52:59 +0200

    fix url exception

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ app.get("/", (_req, res) => {
 const urls = [];
 
 app.get("/api/shorturl/:short_url", function (req, res) {
-  console.log("GET", req.params);
-
   const id = +req.params.short_url;
   const url = urls.find(url => url.short_url === id);
   if (!url) {
@@ -35,24 +33,20 @@ app.get("/api/shorturl/:short_url", function (req, res) {
 // Your first API endpoint
 app.post("/api/shorturl", function (req, res) {
   const original_url = req.body.url;
-
-  console.log("POST", original_url);
-
   try {
     const url = new URL(original_url);
     dns.lookup(url.hostname, err => {
       if (err || !url.protocol.startsWith("http")) {
-        console.log("err", err, url.protocol);
-        return res.status(400).json({ error: "invalid url" });
+        res.json({ error: "invalid url" });
+      } else {
+        const site = { original_url, short_url: urls.length + 1 };
+        urls.push(site);
+
+        res.json(site);
       }
-
-      const site = { original_url, short_url: urls.length + 1 };
-      urls.push(site);
-
-      res.json(site);
     });
   } catch (_ex) {
-    res.status(400).json({ error: "invalid url" });
+    res.json({ error: "invalid url" });
   }
 });
 
